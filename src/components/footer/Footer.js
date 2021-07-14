88a8d6c67a76267dd24b30b4dc613d050074e961
Ashish Salunkhe <avsalunkhe98@gmail.com>
2021-07-14 14:47:41 +0530

    app.js navbar gallery routing changes 2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,26 @@
 import React from "react";
+import {Facebook, Instagram, Mail, Youtube} from 'react-feather'
 
 class Footer extends React.Component {
     render(){
         return(
             <div>
-                <footer class="footer content has-background-black has-text-centered">
+                <footer class="footer has-background-black has-text-centered">
                 
-                    <p>
-                    <strong>Built</strong> by <a href="https://ashishsalunkhe.github.io">Ashish Salunkhe</a> and OSV Tech. Copyrights &copy;
-                    2021 Abhijeet Matkar
+                    
+                    <div className="container social-icons is-mobile center">
+                                <a href="#"><Youtube/></a>
+                                <a href="#"><Facebook/></a>
+                                <a href="#"><Instagram/></a>
+                                <a href="#"><Mail/></a>
+                                <p className="content">
+                                Copyrights &copy; 2021 Abhijeet Matkar             
+                                <br/>   Engagements | Pre-Wedding | 
+                            <br/>Wedding | Food | Baby Shoots</p>
+                        </div> 
+                        <p className=" pt-1 is-mobile center">
+                    Built by <a className="text-dec" href="https://ashishsalunkhe.github.io">Ashish Salunkhe</a> and OSV Tech. 
                     </p>
-                
                 </footer>
             </div>
         );
