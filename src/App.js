c45fc77bdce90ab891ef88662260791709be1346
Ashish Salunkhe <avsalunkhe98@gmail.com>
2021-07-14 11:44:32 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import Wedding from './components/wedding/Wedding';
 import Prewedding from './components/prewedding/Prewedding';
 import Food from './components/food/Food'; 
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-function App() {
-  return (
-    <Router>
-      <Route exact path='/' component={Main}/>
-=======
 import GalleryRouters from "./components/Main.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Gallery from './components/gallery/Gallery';
@@ -38,7 +31,6 @@ function App() {
     <Router>
       <Route exact path='/' component={Main}/>
       {/* <Route path='/gallery' component={Gallery}/> */}
->>>>>>> d6f68aa335fc92f176a8d3511b44e29d1ef7b299
       <Route  path="/babyshoots"   component={Babyshoot} />
       <Route  path="/wedding"   component={Wedding} />
       <Route  path="/prewedding"  component={Prewedding} />
